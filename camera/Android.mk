CyanogenMod/android_device_sony_msm8960-common

move camera wrapper down a level from qcom-common
Change-Id: I8ecd2ac5b61976961a452e4cd6102fc1f5ad66e0

camera/Android.mk
@@ -1,0 +1,20 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    CameraWrapper.cpp
+
+LOCAL_C_INCLUDES := \
+    system/media/camera/include
+
+LOCAL_SHARED_LIBRARIES := \
+    libhardware liblog libcamera_client libutils libcutils
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
+LOCAL_MODULE := camera.msm8960
+
+
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)

msm8960.mk
@@ -17,6 +17,10 @@ $(call inherit-product, device/sony/qcom-common/qcom-common.mk)
 
 COMMON_PATH := device/sony/msm8960-common
 
+# Camera wrapper
+PRODUCT_PACKAGES += \
+    camera.msm8960
+
 # Lights wrapper
 PRODUCT_PACKAGES += \
     lights.msm8960

SlimRoms/device_sony_msm8960-common

camera/Android.mk
@@ -1,0 +1,21 @@
+ifneq ($(TARGET_NEEDS_CAMERA_WRAPPER),false)
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    CameraWrapper.cpp
+
+LOCAL_C_INCLUDES := \
+    system/media/camera/include
+
+LOCAL_SHARED_LIBRARIES := \
+    libhardware liblog libcamera_client libutils libcutils
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
+LOCAL_MODULE := camera.qcom
+
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
+endif

msm8960.mk
@@ -17,6 +17,10 @@ $(call inherit-product, device/sony/qcom-common/qcom-common.mk)
 
 COMMON_PATH := device/sony/msm8960-common
 
+# Camera wrapper
+PRODUCT_PACKAGES += \
+    camera.qcom
+
 # Lights wrapper
 PRODUCT_PACKAGES += \
     lights.qcom