eaa797834abd5bcdc77d1689d8d75e50de677531
olivieer <yves.olli@gmail.com>
2015-05-03 16:45:59 +0200

    msm8960: Rename lights HAL
    Change-Id: I7af4f25ef0e9639a09aa06c3287049b18edeb7d8

--- liblights/Android.mk
+++ liblights/Android.mk
@@ -1,3 +1,4 @@
+ifeq ($(TARGET_PROVIDES_LIBLIGHT),true)
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -12,8 +13,9 @@ LOCAL_SHARED_LIBRARIES := \
     libhardware liblog libutils libcutils
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_MODULE := lights.qcom
+LOCAL_MODULE := lights.msm8960
 
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
+endif
